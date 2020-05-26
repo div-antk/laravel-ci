9edc28ac52b852a1e89918cac6c0175a11e9f71e
div-antk <51283468+owaii@users.noreply.github.com>
2020-05-26 21:49:54 +0900

    ログイン済み状態のケース

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -25,6 +26,19 @@ class ArticleControllerTest extends TestCase
         $response = $this->get(route('articles.create'));
 
         $response->assertRedirect(route('login'));
+    }
+
+    // ログイン済み状態のケース
+    public function testAuthCreate()
+    {
+        $user = factory(User::class)->create();
+
+        // 引数として渡したユーザーモデルにてログインした状態を作り出す
+        $response = $this->actingAs($user)
+            ->get(route('articles.create'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.create');
     }
 
     
